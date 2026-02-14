82b013612fa4bdd7a077f75d59d7a73807b41302
siva sagar <sivasagar1909@gmail.com>
2026-02-15 02:20:41 +0530

    Update login page and logo assets

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -31,8 +31,7 @@ export default function LoginPage() {
                     <button
                         onClick={handleLogin}
                         type="button"
-                        className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                    >
+                        className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                         Sign in with Google
                     </button>
                 </div>
