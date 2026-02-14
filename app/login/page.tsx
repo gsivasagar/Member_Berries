758d7c448f0efe5e3fe6dd50fc1aacdcb775e50e
siva sagar <sivasagar1909@gmail.com>
2026-02-15 02:11:38 +0530

    Remove login footer

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -36,13 +36,6 @@ export default function LoginPage() {
                         Sign in with Google
                     </button>
                 </div>
-
-                <p className="mt-10 text-center text-sm/6 text-gray-400">
-                    Not a member?{' '}
-                    <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-                        Start a 14 day free trial
-                    </a>
-                </p>
             </div>
         </div>
     );
